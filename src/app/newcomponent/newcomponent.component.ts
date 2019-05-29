25eae43f869f4191605342adb5b9b4ef740a9523
qwer <qwer>
2019-05-29 15:55:27 +0300

    NewComponent added

---
+++ src/app/newcomponent/newcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newcomponent',
+  templateUrl: './newcomponent.component.html',
+  styleUrls: ['./newcomponent.component.scss']
+})
+export class NewcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
