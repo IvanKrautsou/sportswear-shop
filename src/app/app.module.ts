bec32f9f10ec2780129f227c6ce8b28028cbc28a
qwer <qwer>
2019-06-11 13:13:06 +0300

    Revert "NewComponent added"
    This reverts commit 25eae43

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewcomponentComponent } from './newcomponent/newcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewcomponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
