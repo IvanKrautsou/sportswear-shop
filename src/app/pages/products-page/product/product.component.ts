55894fb798f23d716fcfc7cb7c0eeed4611c861d
qwer <qwer>
2019-06-12 19:04:04 +0300

    Products images and json

--- src/app/pages/products-page/product/product.component.ts
+++ src/app/pages/products-page/product/product.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  public product = {
+    name: 'T-shirt',
+    price: 'men',
+    size: 'size',
+    img: '../../../../../public/products/T-shirt.png',
+    sex: 'men',
+    id: 1
+  };
+
   constructor() { }
 
   ngOnInit() {
