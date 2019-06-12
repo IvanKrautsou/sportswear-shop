f551e0c12f9ccaf02d0e276cfa4bce9bafa2ff1a
qwer <qwer>
2019-06-12 15:48:11 +0300

    Products-page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { GoodsComponent } from './pages/products-page/goods/goods.component';
+import { ProductsFilterComponent } from './pages/products-page/products-filter/products-filter.component';
+import { AddProductComponent } from './pages/products-page/add-product/add-product.component';
+import { ProductComponent } from './pages/products-page/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsPageComponent,
+    GoodsComponent,
+    ProductsFilterComponent,
+    AddProductComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
