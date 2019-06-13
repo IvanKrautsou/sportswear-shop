8eed477cf13fd718a95ada957f340681e74d5068
qwer <qwer>
2019-06-13 16:13:02 +0300

    display products ngFor

--- src/app/pages/products-page/goods/goods.component.ts
+++ src/app/pages/products-page/goods/goods.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoodsComponent implements OnInit {
 
+  public products = [
+    {
+      name: 'T-shirt',
+      price: 'men',
+      size: 'size',
+      img: '../../../../assets/images/products/T-shirt.png',
+      sex: 'men',
+      id: 1
+    },
+    {
+      name: 'Pants FORCLAZ',
+      price: 'children',
+      size: 'size',
+      img: '../../../../assets/images/products/Pants.png',
+      sex: 'children',
+      id: 2
+    },
+    {
+      name: 'Pants FORCLAZ',
+      price: 'men',
+      size: '10litri',
+      img: '../../../../assets/images/products/backpack.png',
+      sex: 'men',
+      id: 3
+    },
+    {
+      name: 'T-shirt',
+      price: 'women',
+      size: 'size',
+      img: '../../../../assets/images/products/T-shirt.png',
+      sex: 'women',
+      id: 4
+    },
+    {
+      name: 'Pants FORCLAZ',
+      price: 'children',
+      size: '10litri',
+      img: '../../../../assets/images/products/backpack.png',
+      sex: 'children',
+      id: 5
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
